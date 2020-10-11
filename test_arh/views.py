bafe341c9b104c834394d4b6002a98a77600e6ea
levlari <JGA^vbz4>
2020-10-11 23:45:00 +0300

    12

--- test_arh/views.py
+++ test_arh/views.py
@@ -1,24 +1,26 @@
 from django.shortcuts import render,get_object_or_404, redirect
 from .models import Question, Answers
+import datetime
 
 #user_answers = []
 #user_points = 0
 
 def test_arh(request):
-
+    if request.method == 'GET' and request.user.curent_question == 1:
+        request.user.time_begin = datetime.datetime.now().time()
     if request.user.username == 'AnonymousUser':  return redirect('')
     question_ids = Question.objects.all().values_list('id', 'body', 'title','test_id') #,'answers', 'body', 'title')
     question_ids = question_ids.filter(test_id = request.user.curent_test)
 #    print (question_ids.all())
-    if request.user.curent_question > len(question_ids):
+    if request.user.curent_question > len(question_ids) or (timeinsec(request.user.time_begin)+15*60) < timeinsec(datetime.datetime.now().time()):
         return redirect('/final/' )
 #    print('question_id = '+str(request.user.curent_question))
 #    print ('question_len = ' + str(len(question_ids)))
     question =  get_object_or_404(Question, Num = request.user.curent_question, test_id = request.user.curent_test)
-#    print (question.test_id)
+    time_to_live = timeintime (timeinsec(request.user.time_begin)+15*60 -timeinsec(datetime.datetime.now().time()))
     answers = question.answers.filter(active=True)
     correct_answer = answers.filter(correct_answer=True)
-#    print('correct_answer is ' + str(correct_answer[0]))
+
 
     if request.method == 'POST':
 
@@ -33,7 +35,8 @@ def test_arh(request):
 
         request.user.curent_question += 1
 
-#        print('radio is ' + str(request.POST['radio']))
+
+
         if str(request.POST['radio']) == str(correct_answer[0]):
 
             request.user.points += 1# прибавляем счётчик правильных ответов
@@ -54,6 +57,7 @@ def test_arh(request):
                   'test_arh/question.html',
                   {'question': question,
                    'answers': answers,
+                   'time_to_live' : str(time_to_live),
                    }
 )
 
@@ -72,3 +76,10 @@ def test_arh_final (request):
 def test_arh_start (request):
     return render(request,
                   'test_arh/start.html',)
+
+def timeinsec (intime: datetime.time):
+    return intime.hour * 60 * 60 + intime.minute * 60 + intime.second
+
+def timeintime (inseconds: int):
+
+    return datetime.time(hour = inseconds // (60*60), minute= (inseconds % (60*60)) // 60, second= (inseconds % (60*60)) % 60 )
--- users/models.py
+++ users/models.py
@@ -8,3 +8,4 @@ class CustomUser(AbstractUser):
     points = models.IntegerField (default=0)
     curent_question = models.IntegerField (default=1)
     curent_test = models.IntegerField(default=1)
+    time_begin = models.TimeField (auto_now=False, auto_now_add=False, default='00:00:00')
