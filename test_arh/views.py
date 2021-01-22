3ec75a829ac1fc13386eb996b08f9d56a4c9060f
levlari <JGA^vbz4>
2021-01-22 21:54:15 +0300

    p587936.kvmvps debug favicon.ico

--- test_arh/views.py
+++ test_arh/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render,get_object_or_404, redirect
+from django.http import HttpResponse
 from .models import Question, Answers
 import datetime
 
@@ -6,20 +7,20 @@ import datetime
 #user_points = 0
 
 def test_arh(request):
-    if request.method == 'GET' and request.user.curent_question == 1:
-        request.user.time_begin = datetime.datetime.now().time()
+    if request.method == 'GET' and request.user.curent_question == 1: #если 1 вопрос
+        request.user.time_begin = datetime.datetime.now().time() #засекаем время
     if request.user.username == 'AnonymousUser':  return redirect('')
     question_ids = Question.objects.all().values_list('id', 'body', 'title','test_id') #,'answers', 'body', 'title')
     question_ids = question_ids.filter(test_id = request.user.curent_test)
 #    print (question_ids.all())
-    if request.user.curent_question > len(question_ids) or (timeinsec(request.user.time_begin)+10*60) < timeinsec(datetime.datetime.now().time()):
+    if request.user.curent_question > len(question_ids) or (timeinsec(request.user.time_begin)+15*60) < timeinsec(datetime.datetime.now().time()):
         request.user.curent_question = len(question_ids) +1
         request.user.save()
         return redirect('/final/' )
 #    print('question_id = '+str(request.user.curent_question))
 #    print ('question_len = ' + str(len(question_ids)))
     question =  get_object_or_404(Question, Num = request.user.curent_question, test_id = request.user.curent_test)
-    time_to_live = timeintime (timeinsec(request.user.time_begin)+10*60 -timeinsec(datetime.datetime.now().time()))
+    time_to_live = timeintime (timeinsec(request.user.time_begin)+15*60 -timeinsec(datetime.datetime.now().time()))
     answers = question.answers.filter(active=True)
     correct_answer = answers.filter(correct_answer=True)
 
@@ -85,3 +86,7 @@ def timeinsec (intime: datetime.time):
 def timeintime (inseconds: int):
 
     return datetime.time(hour = inseconds // (60*60), minute= (inseconds % (60*60)) // 60, second= (inseconds % (60*60)) % 60 )
+
+def my_image (request):
+    image_data = open("/var/www/arkhangel/favicon.ico", "rb").read()
+    return HttpResponse(image_data, mimetype="image/png")
