7aa7166de2f75a35fc0e72287a3be5dd0317a6d2
Lev <70910988+levlari@users.noreply.github.com>
2021-08-18 21:46:04 +0300

    pep8 test_arh/views

--- test_arh/views.py
+++ test_arh/views.py
@@ -1,98 +1,119 @@
-from django.shortcuts import render,get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
-from .models import Question, Answers, Test
+from .models import Question, Test
 import datetime
 
-#user_answers = []
-#user_points = 0
 
 def test_arh(request):
-    if request.method == 'GET' and request.user.curent_question == 1: #если 1 вопрос
-        request.user.time_begin = datetime.datetime.now().time() #засекаем время
-        request.user.date_begin = datetime.date.today()
-        request.user.save()
-    if request.user.username == 'AnonymousUser':  return redirect('')
-    question_ids = Question.objects.all().values_list('id', 'body', 'title','test_id') #,'answers', 'body', 'title')
-    question_ids = question_ids.filter(test_id = request.user.curent_test)
-    #cur_test = Test.objects.all()#.values_list('test_id', 'TimeToTest')
-    #print (cur_test.all(TimeToTest))
-    cur_test = Test.objects.get(test_id = request.user.curent_test)
+    # проверяем запрос на начало теста
+    if request.method == 'GET' and request.user.curent_question == 1:  # если 1 вопрос
+        request.user.time_begin = datetime.datetime.now().time()  # засекаем время
+        request.user.date_begin = datetime.date.today()  # и дату
+        request.user.save()  # сохраняем
+
+    if request.user.username == 'AnonymousUser':  # не авторизовался
+        return redirect('')
+
+    # получаем набор вопросов для теста пользователя, время на этот тест
+    question_ids = Question.objects.all().values_list('id', 'body', 'title', 'test_id')
+    question_ids = question_ids.filter(test_id=request.user.curent_test)
+    cur_test = Test.objects.get(test_id=request.user.curent_test)
     timetotest = cur_test.TimeToTest
-    print (cur_test.TimeToTest)
-    if request.user.curent_question > len(question_ids) or (timeinsec(request.user.time_begin) + timeinsec(timetotest)) < timeinsec(datetime.datetime.now().time()) or request.user.date_begin < datetime.date.today():
-        request.user.curent_question = len(question_ids) +1
+
+    print(cur_test.TimeToTest)
+
+    # проверяем на законченность тест по вопросам или по времени,
+    # переведя в секунды время начала и время на тест, текущую дату
+    # учитываем смену дня
+    cur_time_in_sec = timeinsec(datetime.datetime.now().time())
+    if request.user.date_begin < datetime.date.today():
+        cur_time_in_sec = cur_time_in_sec + 86400*(datetime.date.today() - request.user.date_begin).days
+    if request.user.curent_question > len(question_ids) or (
+            timeinsec(request.user.time_begin) + timeinsec(timetotest)) < cur_time_in_sec:
+        # увеличиваем счётчик вопросов, и выходим на конечную страницу
+        request.user.curent_question = len(question_ids) + 1
         request.user.save()
-        return redirect('/final/' )
-#    print('question_id = '+str(request.user.curent_question))
-#    print ('question_len = ' + str(len(question_ids)))
-    question =  get_object_or_404(Question, Num = request.user.curent_question, test_id = request.user.curent_test)
-    time_to_live = timeintime (timeinsec(request.user.time_begin) + timeinsec(timetotest) -timeinsec(datetime.datetime.now().time()))
+        return redirect('/final/')
+
+    # получаем объект текущего вопроса,
+    # на основе поля curent_question у запрошенного пользователя
+    # и его номера теста
+    question = get_object_or_404(Question, Num=request.user.curent_question, test_id=request.user.curent_test)
+    # получаем время до конца в формате времени,
+    # а вычисляем время в секундах: (время от начала + время на тест) - текущее время
+    time_to_live = timeintime(timeinsec(request.user.time_begin) +
+                              timeinsec(timetotest) - cur_time_in_sec)
+    # получаем набор активных ответов и правильный ответ
     answers = question.answers.filter(active=True)
     correct_answer = answers.filter(correct_answer=True)
-
-
+    # если пользователь отправил пост(ответил на вопрос),
+    # то пробуем записать в ответы пользователя выставленый radio_button,
+    # увеличиваем указаель на следующий вопрос,
+    # или вернем текущий вопрос с ответами
     if request.method == 'POST':
-
         try:
-            request.user.answers+=str(request.POST['radio']) + '; '
-        except:
+            request.user.answers += str(request.POST['radio']) + '; '
+        except Exception:
             return render(request,
                           'test_arh/question.html',
                           {'question': question,
                            'answers': answers,
                            })
-
         request.user.curent_question += 1
-
-
-
+        # если радио равен правильному ответу
         if str(request.POST['radio']) == str(correct_answer[0]):
+            request.user.points += 1  # прибавляем счётчик правильных ответов
+        request.user.save()
 
-            request.user.points += 1# прибавляем счётчик правильных ответов
-
-
-
-
-#    print('correct_answer is '+ str(correct_answer[0]))
-
-    request.user.save()
+    # если ответили на последний вопрос, возвращаем конечную страницу
     if request.user.curent_question > len(question_ids):
-        return redirect('/final/' )
-#    print('question_id = '+str(request.user.curent_question))
-#    print ('question_len = ' + str(len(question_ids)))
-    question =  get_object_or_404(Question, Num = request.user.curent_question, test_id = request.user.curent_test)
+        return redirect('/final/')
+
+    # получаем вопрос и набор ответов
+    question = get_object_or_404(Question, Num=request.user.curent_question,
+                                 test_id=request.user.curent_test)
     answers = question.answers.filter(active=True)
-    return render(request,
-                  'test_arh/question.html',
-                  {'question': question,
-                   'answers': answers,
-                   'time_to_live' : time_to_live,
-                   }
-)
 
-def test_arh_final (request):
-    if request.user.username == 'AnonymousUser':  return redirect('/')
+    # возвращаем страницу шаблона,
+    # текущий вопрос, ответы
+    # и оставшееся время на тест
+    return render(request, 'test_arh/question.html', {
+        'question': question,
+        'answers': answers,
+        'time_to_live': time_to_live,
+        })
 
 
-    return render(request,
-                  'test_arh/final.html',
-                  {'points': request.user.points,
-                   'answers': request.user.answers,
-                   'username': request.user.username,
-                   }
-                  )
+# запрос финальной страницы
+def test_arh_final(request):
+    # в корень неавторизовавшихся
+    if request.user.username == 'AnonymousUser':
+        return redirect('/')
+    # выводим результаты теста
+    return render(request, 'test_arh/final.html', {
+        'points': request.user.points,
+        'answers': request.user.answers,
+        'username': request.user.username,
+        })
 
-def test_arh_start (request):
-    return render(request,
-                  'test_arh/start.html',)
 
-def timeinsec (intime: datetime.time):
+# открыть стартовую страницу
+def test_arh_start(request):
+    return render(request, 'test_arh/start.html', )
+
+
+# перевод времени из формата(чч:мм:сс) в секунды(число)
+def timeinsec(intime: datetime.time):
     return intime.hour * 60 * 60 + intime.minute * 60 + intime.second
 
-def timeintime (inseconds: int):
 
-    return datetime.time(hour = inseconds // (60*60), minute= (inseconds % (60*60)) // 60, second= (inseconds % (60*60)) % 60 )
+# перевод времени из секунд(число) в формат(чч:мм:сс)
+def timeintime(inseconds: int):
+    return datetime.time(hour=inseconds // (60 * 60), minute=(inseconds % (60 * 60)) // 60,
+                         second=(inseconds % (60 * 60)) % 60)
 
-def my_image (request):
+
+# загружаем favicon.ico
+def my_image(request):
     image_data = open("/var/www/arkhangel/favicon.ico", "rb").read()
     return HttpResponse(image_data, mimetype="image/png")
